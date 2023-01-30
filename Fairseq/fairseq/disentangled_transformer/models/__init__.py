d4fad1287860856e5f271d4707429412e5450a8f
shu-ting-jia <123926152+shu-ting-jia@users.noreply.github.com>
2023-01-30 16:16:58 +0800

    first

---
+++ Fairseq/fairseq/disentangled_transformer/models/__init__.py
@@ -0,0 +1,4 @@
+import sys
+#sys.path.append("/root/changename/RW-Transformers/MT/Fairseq")
+print(sys.path)
+from examples.translation.disentangled_transformer.models.disentangled_transformer import DisentangledTransformerModel
